72da68183b7dbcd40465d640a594425630f950b8
Naufal Adam Noorrahmat <n.adam28895@gmail.com>
2025-07-05 15:44:38 +0700

    Initial commit portofolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Backend Developer", "Frontend Design", "Data Analyst"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
